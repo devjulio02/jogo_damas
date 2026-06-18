96f80ed045a642dd6409db83ab3239e967851f9d
Julio <soares.julio@academico.ifpb.edu.br>
2026-06-18 11:22:47 -0300

    refactor: ajusta inicializacao da interface

--- src/com/damas/App.java
+++ src/com/damas/App.java
@@ -2,13 +2,9 @@ package com.damas;
 
 import com.damas.gui.JanelaPrincipal;
 
-/**
- * Contém a classe principal do jogo de damas
- * @author Arthur Miranda Tavares {@link arthur.miranda@academico.ufpb.br}
-*/
+// Classe principal do jogo de damas.
 public class App {
     public static void main(String args[]) {
-
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new JanelaPrincipal().setVisible(true);
