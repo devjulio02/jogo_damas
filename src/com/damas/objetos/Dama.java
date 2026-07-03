d194f45a269fc6ebd18a04cd10c5bdcad396f5d1
Julio <soares.julio@academico.ifpb.edu.br>
2026-07-02 22:01:01 -0300

    refactor: concentra regras especificas da dama

--- src/com/damas/objetos/Dama.java
+++ src/com/damas/objetos/Dama.java
@@ -1,12 +1,13 @@
 package com.damas.objetos;
 
-// Representa uma dama e sobrescreve apenas o movimento específico.
+//Representa uma Dama e implementa somente suas regras específicas;
 public class Dama extends PecaAbstrata {
 
     public Dama(Casa casa, CorPeca cor) {
         super(casa, cor);
     }
 
+    //A Dama pode andar várias casas, desde que continue na diagonal;
     @Override
     public boolean isMovimentoValido(Casa destino) {
         int distanciaX = Math.abs(destino.getX() - casa.getX());
@@ -15,6 +16,7 @@ public class Dama extends PecaAbstrata {
         return distanciaX == distanciaY;
     }
 
+    //Sem captura a Dama pode andar em qualquer sentido da diagonal;
     @Override
     public boolean podeMoverSemCaptura(Casa destino) {
         return isMovimentoValido(destino);
