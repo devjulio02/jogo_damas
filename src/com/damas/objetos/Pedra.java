78a9ec7f57016569ac1c734096569e28f2c05438
Julio <soares.julio@academico.ifpb.edu.br>
2026-07-02 22:00:54 -0300

    refactor: concentra regras especificas da pedra

--- src/com/damas/objetos/Pedra.java
+++ src/com/damas/objetos/Pedra.java
@@ -1,12 +1,13 @@
 package com.damas.objetos;
 
-// Representa uma pedra comum.
+//Representa uma Pedra e implementa somente suas regras específicas;
 public class Pedra extends PecaAbstrata {
 
     public Pedra(Casa casa, CorPeca cor) {
         super(casa, cor);
     }
 
+    //A Pedra pode andar no máximo duas casas na diagonal;
     @Override
     public boolean isMovimentoValido(Casa destino) {
         int distanciaX = Math.abs(destino.getX() - casa.getX());
@@ -17,6 +18,7 @@ public class Pedra extends PecaAbstrata {
         return distanciaX <= 2 && distanciaX == distanciaY;
     }
 
+    //Sem captura a Pedra anda uma casa no sentido definido pela sua cor;
     @Override
     public boolean podeMoverSemCaptura(Casa destino) {
         int distanciaX = Math.abs(destino.getX() - casa.getX());
